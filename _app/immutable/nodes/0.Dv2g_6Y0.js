b68b5ded4b5eef1eb130bf52658872d09bd70b32
Deric Mendez <deric@crema.us>
2025-04-24 14:34:29 -0500

    Updates

---
+++ _app/immutable/nodes/0.Dv2g_6Y0.js
@@ -0,0 +1,1 @@
+import{c as n,a}from"../chunks/8m-cJVxX.js";import{p}from"../chunks/DGiw5uL7.js";import{s}from"../chunks/BBZUm6iT.js";const i=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=p(e);s(t,()=>r.children),a(o,e)}export{u as component,f as universal};
